f1b3f046b3af5673fb1d0b6554b49a25eba34395
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-12-07 03:49:32 +0530

    fix: build error

--- trpc/context.ts
+++ trpc/context.ts
@@ -1,12 +1,6 @@
-import { createTRPCContext } from "@trpc/server";
-import { type inferAsyncReturnType } from "@trpc/server";
-
 export const createContext = async () => {
-    const session = await getSession();
     const ctx = {
-        session,
     }
-
     return ctx;
 };
 
